a5891494b9ae7b3fb507f9730c335536be09ff4a
4SAMU <samuelnzomo4@gmail.com>
2023-07-05 19:20:22 +0300

    update

---
+++ components/Start/Start.jsx
@@ -0,0 +1,15 @@
+import styles from "./Start.module.css";
+
+const Start = () => {
+  const { StartPage } = styles;
+  return (
+    <div className={StartPage}>
+      <h1>
+        We back visionary teams building the next generation of decentralized
+        technologies
+      </h1>
+    </div>
+  );
+};
+
+export default Start;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
+import Start from "@/components/Start/Start";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Start />
     </>
   );
 }
