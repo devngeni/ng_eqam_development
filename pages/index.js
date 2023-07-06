34c18436bae440114eb71edb09fe379f1003259a
4SAMU <samuelnzomo4@gmail.com>
2023-07-06 20:23:38 +0300

    update head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import HeadMetaData from "@/components/HeadMetaData";
 import Navbar from "@/components/Navbar/Navbar";
 import Start from "@/components/Start/Start";
 
 export default function Home() {
   return (
     <>
+      <HeadMetaData />
       <Navbar />
       <Start />
     </>
