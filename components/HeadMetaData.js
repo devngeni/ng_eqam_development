f6b91c3b8ba3e496feb490d9c137f4f957c6950e
4SAMU <samuelnzomo4@gmail.com>
2023-07-06 20:20:01 +0300

    update

--- components/HeadMetaData.js
+++ components/HeadMetaData.js
@@ -18,7 +18,16 @@ const HeadMetaData = () => {
           content="Building the Future of Web3 Technology in East Africa and Beyond"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/EQLogo.png" />
+
+        <meta property="og:title" content={pageTitle} />
+        <meta
+          property="og:description"
+          content="Building the Future of Web3 Technology in East Africa and Beyond"
+        />
+        <meta property="og:url" content={router.pathname} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/EQLogo.png" />
       </Head>
     </>
   );
