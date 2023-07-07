7ecb2a54e78ceeaf682bbd3427bea24c5b99b31b
ngunjiri <owen2ngunjiri@gmail.com>
2023-07-07 14:28:52 +0300

    update

--- pages/Jobs.js
+++ pages/Jobs.js
@@ -2,17 +2,19 @@ import React from "react";
 import style from "../styles/Jobs.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
+import Footer from "@/components/Footer/Footer";
 const Jobs = () => {
   return (
     <>
       <Navbar />
-      <div className={style.container}>
-        <div className={style.content}>
+      <div className="ContainerWrapper">
+        <div className={style.container}>
           <div className={style.content_head}>Jobs</div>
+          <div className={style.Rec1}>
+            <div className={style.Text2}>Please check back next time!</div>
+          </div>
         </div>
-        <div className={style.Rec1}>
-          <div className={style.Text2}>Please check back next time!</div>
-        </div>
+        <Footer />
       </div>
     </>
   );
