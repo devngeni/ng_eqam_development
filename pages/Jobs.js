995a84613455c986ed55d2b01808f9c7b8189c4b
ronkips <kipshillash@gmail.com>
2023-07-09 10:59:40 +0300

    updated the job component

--- pages/Jobs.js
+++ pages/Jobs.js
@@ -3,11 +3,16 @@ import style from "../styles/Jobs.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 import Footer from "@/components/Footer/Footer";
+import EQlogo from "@/public/EQLogo.png";
+
 const Jobs = () => {
   return (
     <>
       <Navbar />
       <div className="ContainerWrapper">
+        <div className="blog_logo">
+          <Image className="logo" src={EQlogo} alt="EqLogo" />
+        </div>
         <div className={style.container}>
           <div className={style.content_head}>Jobs</div>
           <div className={style.Rec1}>
