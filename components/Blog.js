1b7a8447e7d38d0bd9d0ca817f6181d86e505bb2
ronkips <kipshillash@gmail.com>
2023-07-10 12:31:42 +0300

    blog update

--- components/Blog.js
+++ components/Blog.js
@@ -1,6 +1,9 @@
-export const blog = [
+export const blogs = [
   {
-    heading: "Introducing Convergence: The Governance Blackhole",
-    content: ""
+    heading: "Convergence: A New Governance Paradigm in DeFi",
+    date: "July 10 2023",
+    author: "I Yaoher Pang",
+    content:
+      "Introducing Convergence: The Governance Blackhole ‍ DeFi has been a  cornerstone of the web3 industry, ranging from basic features such engineering products within the Curve ecosystem. The industry as a straddling the fine line of revolutionary financial products and Convergence and the new ideas they bring to the DeFI ecosystem. Convergence aims to be a “Governance Blackhole” where they accrue Introducing Convergence: The Governance Blackhole ‍ DeFi has been a  cornerstone of the web3 industry, ranging from basic features such engineering products within the Curve ecosystem. The industry as a straddling the fine line of revolutionary financial products an"
   }
 ];
