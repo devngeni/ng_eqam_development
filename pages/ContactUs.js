df1aefdda06192bec9ebed811bbc898453bf8253
ngunjiri <owen2ngunjiri@gmail.com>
2023-10-11 13:25:59 +0300

    updates

--- pages/ContactUs.js
+++ pages/ContactUs.js
@@ -5,8 +5,10 @@ import Footer from "@/components/Footer/Footer";
 import EQlogo from "@/public/EQLogo.png";
 import Image from "next/image";
 
+
 const ContactUs = () => {
-  const { contact_container, contact_heading, paragragh, content } = style;
+  const { contact_container, contact_heading, paragragh, content, nav_links, phone, Email, LinkedIn, Twitter, Medium,
+  Substack } = style;
   return (
     <>
       <Navbar />
@@ -30,6 +32,26 @@ const ContactUs = () => {
               <p>
                 Our investment philosophy centers around the core infrastructure of the blockchain, with a particular focus on initiatives that enhance and facilitate crypto trading.
               </p>
+            </div>
+            <div className={nav_links}>
+            <Link href="tel:+254 742 740 432" target="_blank" rel="noopener noreferrer">
+              <div className={phone}></div>
+              </Link>
+            <Link href="mailto:info@eqcap.fund" target="_blank" rel="noopener noreferrer">
+              <div className={Email}></div>
+              </Link>
+            <Link href="https://www.linkedin.com/company/eqam-capital/" target="_blank" rel="noopener noreferrer">
+              <div className={LinkedIn}></div>
+              </Link>
+            <Link href="https://twitter.com/eQamCap" target="_blank" rel="noopener noreferrer">
+              <div className={Twitter}></div>
+              </Link>
+            <Link href="https://medium.com/@eqcap" target="_blank" rel="noopener noreferrer">
+              <div className={Medium}></div>
+              </Link>
+            <Link href="https://substack.com/@eqamquants" target="_blank" rel="noopener noreferrer">
+              <div className={Substack}></div>
+              </Link>
             </div>
           </div>
         </div>
